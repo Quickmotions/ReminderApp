573e7c2c9a0541b80c8ce606ffc43485e7824495
Fergus <fergusbhaak@gmail.com>
2021-07-25 16:47:08 +0100

    current date not syncing

--- Program/main.py
+++ Program/main.py
@@ -1,16 +1,14 @@
-from pprint import pprint
+
 from time import sleep
 import threading as th
 from datetime import datetime
-import os
-from typing import TextIO
-
+# dependencies
+import notify
 
 class FileManager:
     def __init__(self, f_line=None, create_new=False):
         if create_new:
             self.create_new()
-
         else:
             f_line = f_line.strip('\n').split(',')
             self.reminder = f_line[0]
@@ -22,11 +20,17 @@ class FileManager:
         print('Create a new Reminder:')
         self.reminder = input('Reminder Name: ')
         self.time = input('Reminder Time (HH:MM): ')
-        self.date = input('Reminder Date (YYYY-MM-DD): ')
+        self.date = input('Reminder Date (YY-MM-DD): ')
         print('Reminder Repeat Options:\n-hourly\n-daily\n-weekly\n-monthly\n-yearly,')
         self.repeat = input('Reminder Repeat: ')
         with open('Storage\\reminders.csv', 'a') as f:
             f.write(f'{self.reminder},{self.time},{self.date},{self.repeat}\n')
+
+    def update_reminder_times(self):
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        current_date = now.date()
+        # if self.time < current_time and self.date < current_date:
 
 
 
@@ -35,14 +39,21 @@ def time_manager(r_list):
         now = datetime.now()
         current_time = now.strftime("%H:%M")
         current_date = now.date()
-        # print(current_time, current_date)
+        print(f'{current_time}{current_date}')
         check_reminders(r_list, current_time, current_date)
         sleep(60)
 
 
 def check_reminders(r_list, current_time, current_date):
     for r in r_list:
+        sleep(0.5)
+        print('-------------')
+        print(f'{r.date}\n{current_date}')
+        print('is equal: ', str(r.date) == str(current_time))
+
         if r.time == current_time and current_date == r.date:
+            print('true')
+            notify.notification(f'Reminder: for {r.reminder} at {r.time}', title=f'{r.reminder} at {r.time}')
             print(r.reminder)
 
 
@@ -50,6 +61,7 @@ if __name__ == '__main__':
     reminders = []
     with open('Storage\\reminders.csv', 'r') as f:
         for line in f.readlines():
+
             reminders.append(FileManager(line, False))
     time_thread = th.Thread(target=time_manager, args=[reminders])
     time_thread.start()
