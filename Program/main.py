1574a62979e30c0eb948735b54858ffc9b30679c
Fergus <fergusbhaak@gmail.com>
2021-07-25 15:58:13 +0100

    Created  basic file management and creation of new reminders. Also added 60 sec thread timer

---
+++ Program/main.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+from time import sleep
+import threading as th
+from datetime import datetime
+import os
+from typing import TextIO
+
+
+class FileManager:
+    def __init__(self, f_line=None, create_new=False):
+        if create_new:
+            self.create_new()
+
+        else:
+            f_line = f_line.strip('\n').split(',')
+            self.reminder = f_line[0]
+            self.time = f_line[1]
+            self.date = f_line[2]
+            self.repeat = f_line[3]
+
+    def create_new(self):
+        print('Create a new Reminder:')
+        self.reminder = input('Reminder Name: ')
+        self.time = input('Reminder Time (HH:MM): ')
+        self.date = input('Reminder Date (YYYY-MM-DD): ')
+        print('Reminder Repeat Options:\n-hourly\n-daily\n-weekly\n-monthly\n-yearly,')
+        self.repeat = input('Reminder Repeat: ')
+        with open('Storage\\reminders.csv', 'a') as f:
+            f.write(f'{self.reminder},{self.time},{self.date},{self.repeat}\n')
+
+
+
+def time_manager(r_list):
+    while True:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        current_date = now.date()
+        # print(current_time, current_date)
+        check_reminders(r_list, current_time, current_date)
+        sleep(60)
+
+
+def check_reminders(r_list, current_time, current_date):
+    for r in r_list:
+        if r.time == current_time and current_date == r.date:
+            print(r.reminder)
+
+
+if __name__ == '__main__':
+    reminders = []
+    with open('Storage\\reminders.csv', 'r') as f:
+        for line in f.readlines():
+            reminders.append(FileManager(line, False))
+    time_thread = th.Thread(target=time_manager, args=[reminders])
+    time_thread.start()
+    while True:
+        user_input = input('Options:\n-add\n-remove\n-edit\n-view\n: ')
+        if user_input.lower() == 'add' or user_input == '1':
+            reminders.append(FileManager(None, True))
