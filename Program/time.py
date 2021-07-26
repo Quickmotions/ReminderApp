79da1a4328e1ce648754d3802bd8c81eb0c5b207
Fergus <fergusbhaak@gmail.com>
2021-07-26 15:11:49 +0100

    added pass statments to empty if fucntions as temporary fix

--- Program/time.py
+++ Program/time.py
@@ -12,12 +12,12 @@ def update_reminder_times(initial_time ,initial_date ,repeats):
                 initial_time = '00' + initial_time[2:]
     while int(initial_time[:1]) <= int(current_time[:1]) and int(initial_time[3:]) <= int(current_time[3:]): # do for dates
     if repeats == 'daily':
-        if int(initial_date[8:]) in [] # months with 30
+        if int(initial_date[8:]) in []: # months with 30
             if int(initial_date[8:]) < 30:  # have to add diffrence depending on months maybe mo0ve to new file
                 initial_date = initial_date[:8] + str(int(initial_date[8:]) + 1)
-        if int(initial_date[8:]) in []  # months with 31
-
-        if int(initial_date[8:]) in []  # February with 28
-
+        if int(initial_date[8:]) in []:  # months with 31
+            pass
+        if int(initial_date[8:]) in []:  # February with 28
+            pass
 # 2021-07-25
 # 22:12
