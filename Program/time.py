33bae8c36d9d21ef1dbb7f5f1fa4922ab623965b
Fergus <fergusbhaak@gmail.com>
2021-07-27 12:01:26 +0100

    reorginised time.py with function layout

--- Program/time.py
+++ Program/time.py
@@ -6,12 +6,23 @@ def update_reminder_times(initial_time ,initial_date ,repeats):
     current_date = str(now.date())
     while int(initial_time[:1]) <= int(current_time[:1]) and int(initial_time[3:]) <= int(current_time[3:]):
         if repeats == 'hourly':
-            if int(initial_time[:2]) < 23:
-                initial_time = str(int(initial_time[:2]) + 1) + initial_time[2:]
-            else:
-                initial_time = '00' + initial_time[2:]
+            increase_hour(initial_time ,initial_date)
     while int(initial_time[:1]) <= int(current_time[:1]) and int(initial_time[3:]) <= int(current_time[3:]): # do for dates
-    if repeats == 'daily':
+        if repeats == 'daily':
+            increase_day(initial_time ,initial_date)
+
+# 2021-07-25
+# 22:12
+def increase_hour(initial_time ,initial_date ):
+
+        if int(initial_time[:2]) < 23:
+            initial_time = str(int(initial_time[:2]) + 1) + initial_time[2:]
+        else:
+            initial_time = '00' + initial_time[2:]
+
+
+def increase_day(initial_time ,initial_date ):
+
         if int(initial_date[8:]) in []: # months with 30
             if int(initial_date[8:]) < 30:  # have to add diffrence depending on months maybe mo0ve to new file
                 initial_date = initial_date[:8] + str(int(initial_date[8:]) + 1)
@@ -19,5 +30,10 @@ def update_reminder_times(initial_time ,initial_date ,repeats):
             pass
         if int(initial_date[8:]) in []:  # February with 28
             pass
-# 2021-07-25
-# 22:12
+
+
+def increase_month(initial_time ,initial_date ):
+
+
+
+def increase_year(initial_time ,initial_date ):
