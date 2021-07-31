baa5fa8f77b7f8d5a1da70cedbb7713134bcc812
Fergus <fergusbhaak@gmail.com>
2021-07-31 15:24:06 +0100

    new updates: changing seperated time/date into datetime objects allowing for changing time, renamed time.py to stop it conflicing with time.sleep library, now storing datetime objects of reminders so that strftime can be accessed to get time and date strings

--- Program/main.py
+++ Program/main.py
@@ -1,5 +1,5 @@
 # other files
-from time import update_reminder_times, update_reminder_dates
+from time_manage import update_reminder_times
 # built in imports
 from time import sleep
 import threading as th
@@ -7,13 +7,14 @@ from datetime import datetime
 # dependencies
 from pynotifier import Notification
 
+
 class FileManager:
     def __init__(self, f_line=None, create_new=False):
         if create_new:
             self.create_new()
         else:
             f_line = f_line.strip('\n').split(',')
-            self.reminder = f_line[0]
+            self.name = f_line[0]
             self.time = f_line[1]
             self.date = f_line[2]
             self.repeat = f_line[3]
@@ -22,48 +23,50 @@ class FileManager:
             print(self.date)
 
     def create_new(self):
-        print('Create a new Reminder:')
-        self.reminder = input('Reminder Name: ')
+        print('Create a new Reminder:') # need to convert date and time into datetime object
+        self.name = input('Reminder Name: ')
         self.time = input('Reminder Time (HH:MM): ')
-        self.date = input('Reminder Date (YY-MM-DD): ')
+        self.date = input('Reminder Date (YY:MM:DD): ')
         print('Reminder Repeat Options:\n-hourly\n-daily\n-weekly\n-monthly\n-yearly,')
         self.repeat = input('Reminder Repeat: ')
         with open('Storage\\reminders.csv', 'a') as f:
-            f.write(f'{self.reminder},{self.time},{self.date},{self.repeat}\n')
-
+            f.write(f'{self.name},{self.time},{self.date},{self.repeat}\n')
 
 
 def time_manager(r_list):
     while True:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M")
-        current_date = now.date()
-        check_reminders(r_list, current_time, current_date)
-        # update reminders
-        for reminder in reminders:
-            reminder.time = update_reminder_times(reminder.time, reminder.date, reminder.repeat)
+        current_datetime = datetime.now()
+        # print time and date
+        print(current_datetime.strftime('%H:%M'), current_datetime.strftime('%Y:%m:%d'))
+        # check times
+        check_reminders(r_list, current_datetime)
         # check each min
         sleep(60)
 
 
-def check_reminders(r_list, current_time, current_date):
-    for r in r_list:
-        sleep(0.5)
-        if r.time == str(current_time) and r.date == str(current_date):
+def check_reminders(r_list, datetime_now):
+
+    # update timers that have expired
+    for reminder in r_list:
+        reminder.update_reminder_times(reminder.) # need to insert date time object and assign output to class object
+
+    # check reminders against current time/date
+    for reminder in r_list:
+        if reminder.time == datetime_now.strftime('%H:%M') and reminder.date == datetime_now.strftime('%Y:%m:%d'):
+            # send customised notification
             Notification(
-                title=f'{r.reminder} at {r.time}',
-                description=f'Reminder: {r.reminder} at {r.time}, notified on a {r.repeat} basis',
+                title=f'{reminder.name} at {reminder.time}',
+                description=f'Reminder: {reminder.reminder} at {reminder.time}, notified on a {r.repeat} basis',
                 duration=10,  # Duration in seconds
                 urgency='normal'
             ).send()
-            r.update_reminder_times()
+            print('DEBUG', reminder.name, reminder.time, reminder.date)
 
 
 if __name__ == '__main__':
     reminders = []
     with open('Storage\\reminders.csv', 'r') as f:
         for line in f.readlines():
-
             reminders.append(FileManager(line, False))
     time_thread = th.Thread(target=time_manager, args=[reminders])
     time_thread.start()
---
+++ Program/time_manage.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+
+def update_reminder_times(initial_datetime, repeats):
+    now = datetime.now()
+    current_time = str(now.strftime("%H:%M"))
+    current_date = str(now.strftime('%Y:%m:%d'))
+
+
+def hourly(initial_datetime):
+
+
+    return new_datetime
+
+
+def daily(initial_datetime):
+    return new_datetime
+
+
+def monthly(initial_datetime):
+    return new_datetime
+
+
+def yearly(initial_datetime):
+    return new_datetime
+
+
+# now = datetime.now() + timedelta(hours=10)
+# print(now.strftime('%H:%M'))
+# print(now.strftime('%Y:%m:%d'))
